3830ba4a0248051fd0b533e36be8661c359793e6
Susmit-A <susmit600@gmail.com>
2019-01-19 12:45:39 +0530

    Added MapmyIndia API

--- Android/app/src/main/java/com/excelsior/mshack/MainActivity.kt
+++ Android/app/src/main/java/com/excelsior/mshack/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.excelsior.mshack
 
-import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mmi.LicenceManager
+import com.mmi.MapmyIndiaMapView
+import com.mmi.util.GeoPoint
 
-class MainActivity : ActionBarActivity() {
+
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LicenceManager.getInstance().mapSDKKey = "my7yci3zby43mtrz443ffnjuqs7t8dm7"
+        LicenceManager.getInstance().restAPIKey = "r7hd1ec5bhw5rned2bd4cc9r44a1q46t"
+
+        val mapmyIndiaMapView = findViewById<MapmyIndiaMapView>(R.id.map)
+        val mapView = mapmyIndiaMapView.mapView
+
+        val geoPoint = GeoPoint(48.8583, 2.0, 2944.0)
+        mapView.setCenter(geoPoint)
     }
+
 }
