af9e5f185f80ec4c9ac84b4e0c005194cf2b3d6b
Vishal-V <vishal114186@gmail.com>
2019-01-19 13:19:21 +0530

    Added all routes and UI

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app.config['SECRET_KEY'] = 'a551d32359baf371b9095f28d45347c8b8621830'
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template('index.html', title='Home')
+
+@app.route('/trip', methods=['GET', 'POST'])
+def trips():
+	return render_template('trips.html', title='Trips')
+
+@app.route('/POIs', methods=['GET', 'POST'])
+def pois():
+	return render_template('pois.html', title='POIs')
 
 app.run(debug=True)
